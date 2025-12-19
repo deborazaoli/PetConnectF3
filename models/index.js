08cd322ad25b438230fc4f3bbdd12e53e010f21f
Débora Oliveira <debora.oliveira2138@gmail.com>
2025-12-18 21:34:15 -0300

    Refactor model associations in index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const sequelize = require('../config/database');
 
-// Importar os modelos
 const Tutor = require('./tutor.model');
 const Pet = require('./pet.model');
 const Prestador = require('./prestador.model');
@@ -9,23 +8,15 @@ const Servico = require('./servico.model');
 const Agendamento = require('./agendamento.model');
 const Prontuario = require('./prontuario.model');
 
-// --- ASSOCIAÇÕES BIDIRECIONAIS ---
-
-// 1. Tutor <-> Pet
 Tutor.hasMany(Pet, { foreignKey: 'tutorId', onDelete: 'CASCADE' });
 Pet.belongsTo(Tutor, { foreignKey: 'tutorId' });
 
-// 2. Pet <-> Prontuario
 Pet.hasMany(Prontuario, { foreignKey: 'petId', onDelete: 'CASCADE' });
 Prontuario.belongsTo(Pet, { foreignKey: 'petId' });
 
-// --- OUTRAS ASSOCIAÇÕES ---
-
-// Veterinário assina o prontuário
 Veterinario.hasMany(Prontuario, { foreignKey: 'veterinarioId' });
 Prontuario.belongsTo(Veterinario, { foreignKey: 'veterinarioId' });
 
-// Agendamento relações
 Pet.hasMany(Agendamento, { foreignKey: 'petId' });
 Agendamento.belongsTo(Pet, { foreignKey: 'petId' });
 
@@ -44,4 +35,5 @@ module.exports = {
     Servico,
     Agendamento,
     Prontuario
+
 };
